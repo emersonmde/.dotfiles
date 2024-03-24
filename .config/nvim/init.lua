a18506212391c851d82c7192aee0e3935d6806c6
Matthew Emerson <emersonmde@protonmail.com>
2024-03-24 18:13:50 -0400

    Added goto next usage in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -576,6 +576,19 @@ vim.defer_fn(function()
         },
       },
     },
+    refactor = {
+      navigation = {
+        enable = true,
+        -- Assign keymaps to false to disable them, e.g. `goto_definition = false`.
+        keymaps = {
+          goto_definition = "gnd",
+          list_definitions = "gnD",
+          list_definitions_toc = "gO",
+          goto_next_usage = "<a-*>",
+          goto_previous_usage = "<a-#>",
+        },
+      },
+    },
   }
 end, 0)
 
